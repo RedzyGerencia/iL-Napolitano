9746dd53ca46fb8bfe5d3ed82f891b9bbc53a4f4
ArquitectoIA <arquitectoia@agenciaredzilla.com>
2025-08-22 13:54:28 -0500

    Pruebas

--- src/iLNapolitano.js
+++ src/iLNapolitano.js
@@ -1,4 +1,5 @@
 import { iLNapolitanoProductos } from "./iLNapolitanoProductos.js";
+import { selector_cantidades } from "./components.js";
 
 const SCREEN_RESPONSES = {
   ENTRADAS: {
@@ -14,56 +15,7 @@ const SCREEN_RESPONSES = {
       chk_entrada_8: false,
       chk_entrada_9: false,
       chk_entrada_10: false,
-      selector_cantidades: [
-        {
-          id: "1",
-          title: "01",
-        },
-        {
-          id: "2",
-          title: "02",
-        },
-        {
-          id: "3",
-          title: "03",
-        },
-        {
-          id: "4",
-          title: "04",
-        },
-        {
-          id: "5",
-          title: "05",
-        },
-        {
-          id: "6",
-          title: "06",
-        },
-        {
-          id: "7",
-          title: "07",
-        },
-        {
-          id: "8",
-          title: "08",
-        },
-        {
-          id: "9",
-          title: "09",
-        },
-        {
-          id: "10",
-          title: "10",
-        },
-        {
-          id: "11",
-          title: "11",
-        },
-        {
-          id: "12",
-          title: "12",
-        },
-      ],
+      selector_cantidades,
     },
   },
 
@@ -152,6 +104,7 @@ const SCREEN_RESPONSES = {
       chk_sug_36: false,
       chk_sug_37: false,
       chk_sug_38: false,
+      selector_cantidades,
     },
   },
 
