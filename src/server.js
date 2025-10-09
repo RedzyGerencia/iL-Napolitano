cc54cef3abe7ff74aafebe159f75035bcf9a30d7
ArquitectoIA <arquitectoia@agenciaredzilla.com>
2025-10-09 13:34:23 -0500

    Cambio a pietres pizza

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import {
   encryptResponse,
   FlowEndpointException,
 } from "./encryption.js";
-import { getNextScreen } from "./mr_gloton/mr_gloton.js";
+import { getNextScreen } from "./pietres_pizza/pietres_pizza.js";
 import crypto from "crypto";
 
 const app = express();
